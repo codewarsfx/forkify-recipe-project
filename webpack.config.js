9df2e203d2c6b85ac9f3749ae0aba80fcaa3ae7c
codewarsfx <chiderainnocent001@gmail.com>
2020-05-01 19:49:21 +0100

    reconfigured webpack to match with new distribution directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const htmlwebpackplugin = require('html-webpack-plugin');
 module.exports = {
   entry: ['babel-polyfill', './src/js/index.js'],
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './distr'),
     filename: 'js/bundle.js',
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: './distr',
   },
   plugins: [
     new htmlwebpackplugin({
